50512de8aef39a26e3d10209d138f6453c0da87b
henriqueacf <henrique.assis145@gmail.com>
2024-10-17 11:10:26 -0300

    feat: endpoint de delete activity

--- src/Journey.Api/Controllers/TripsController.cs
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -1,4 +1,5 @@
 ﻿using Journey.Application.UseCases.Activities.Complete;
+using Journey.Application.UseCases.Activities.Delete;
 using Journey.Application.UseCases.Activities.Register;
 using Journey.Application.UseCases.Trips.Delete;
 using Journey.Application.UseCases.Trips.GetAll;
@@ -91,6 +92,22 @@ public class TripsController : ControllerBase
         [FromRoute] Guid activityId)
     {
         var useCase = new CompleteActivityForTripUseCase();
+        
+        useCase.Execute(tripId, activityId);
+        
+        return NoContent();
+    }
+    
+    // DELETE ACTIVITY
+    [HttpDelete]
+    [Route("{tripId}/activity/{activityId}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+    public IActionResult DeleteActivity(
+        [FromRoute] Guid tripId,
+        [FromRoute] Guid activityId)
+    {
+        var useCase = new DeleteActivityForTripUseCase();
         
         useCase.Execute(tripId, activityId);
         
